5c68a68d170cc1ff550dd217745ab442ac0e811e
vrindad <vrindadubey.vd@gmail.com>
2025-05-26 09:54:29 +0530

    Create 253-MeetingRoomsII.cpp

---
+++ 253-MeetingRoomsII.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int minMeetingRooms(vector<vector<int>>& intervals) {
+    if (intervals.empty()) return 0;
+
+    sort(intervals.begin(), intervals.end());
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    minHeap.push(intervals[0][1]);
+
+    for (int i = 1; i < intervals.size(); i++) {
+        if (intervals[i][0] >= minHeap.top())
+            minHeap.pop();
+
+        minHeap.push(intervals[i][1]);
+    }
+
+    return minHeap.size();
+}
