04786288fe3df48f0f92c420daf1bef999f37f78
vrindad <vrindadubey.vd@gmail.com>
2025-05-26 10:40:35 +0530

    Create 938-RangeSumOfBST.cpp

---
+++ 938-RangeSumOfBST.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        int sum = 0;
+        if (root->val >= low && root->val <= high) {
+            sum += root->val;
+        }
+        if (root->val > low) {
+            sum += rangeSumBST(root->left, low, high);
+        }
+        if (root->val < high) {
+            sum += rangeSumBST(root->right, low, high);
+        }
+        return sum;
+    }
+};
