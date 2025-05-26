f916f1982ab855df2b212d4b0dafb18a7d8a2b03
vrindad <vrindadubey.vd@gmail.com>
2025-05-26 09:59:04 +0530

    Create 373-FindKPairsWithSmallestSum.cpp

---
+++ 373-FindKPairsWithSmallestSum.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+    vector<vector<int>> result;
+    if (nums1.empty() || nums2.empty() || k == 0)
+        return result;
+
+    auto cmp = [&](const pair<int, int>& a, const pair<int, int>& b) {
+        return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];
+    };
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> minHeap(cmp);
+    for (int i = 0; i < nums1.size() && i < k; i++)
+        minHeap.emplace(i, 0);
+
+    while (!minHeap.empty() && result.size() < k) {
+        auto [i, j] = minHeap.top();
+        minHeap.pop();
+        result.push_back({nums1[i], nums2[j]});
+        if (j + 1 < nums2.size())
+            minHeap.emplace(i, j + 1);
+    }
+
+    return result;
+}
